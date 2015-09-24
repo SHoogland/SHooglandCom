53d62989b7277b6eca280a3b128005bd42190581
Stephan Hoogland <stephan@shoogland.com>
2015-09-24 22:56:05 +0200

    activated uglify

--- grunt/uglify.js
+++ grunt/uglify.js
@@ -1,5 +1,5 @@
 module.exports = {
-	demo: {
+	dist: {
 		files: [{
 			'<%= config.dist.root %>shooglandcom.js': ['<%= config.dist.root %>shooglandcom.js'],
 			'<%= config.dist.root %>vendor.js': ['<%= config.dist.root %>vendor.js']
